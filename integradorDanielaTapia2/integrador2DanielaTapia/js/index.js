6667c0d50b993f49ffd772e59e30d067695a605e
Piggypink8 <73083105+Piggypink8@users.noreply.github.com>
2021-10-18 15:42:40 -0300

    Update index.js

--- integradorDanielaTapia2/integrador2DanielaTapia/js/index.js
+++ integradorDanielaTapia2/integrador2DanielaTapia/js/index.js
@@ -1,117 +1,53 @@
-/*document.getElementById("hola").innerHTML = "Bienvenidos a DOM!";
+/*Función para tomar los datos ingresados y dar el valor total teniendo en cuenta la categoría y cantidad*/
 
+function totalSummary() {
 
-
-
-function crearNota() {
-    // modificación contenido html
-    document.getElementById("nota").innerHTML = "Bienvenido DOM"
-
-    document.getElementById("btnCrearNota").value = "BORRAR Nota"
-    document.getElementById("btnCrearNota").onclick = borrarNota;
-
-}
-
-function borrarNota() {
-    // modificación contenido html
-    document.getElementById("nota").innerHTML = ""
-
-    document.getElementById("btnCrearNota").value = "CREAR Nota"
-    document.getElementById("btnCrearNota").onclick = crearNota;
-
-}
-
-
-// modifiación estilo css
-function cambiarColorNota() {
-    document.getElementById("nota").style.color = "blue";
-}
-
-//var x = document.getElementsByTagName("p");
-//var x = document.getElementsByClassName("intro");
-// var x = document.querySelectorAll("p.intro");
-// document.getElementById("p2").style.color = "blue";
-// buscar por nombre etiqueta
-var listaP = document.getElementsByTagName("p");
-console.log(listaP);
-
-// buscar por nombre de clase
-var listaClase = document.getElementsByClassName("textoPrincipal");
-console.log(listaClase);
-
-//animaciones
-
-/** EVENTOS 
-    suceso que ocurre en la ventada del navegador
-*/
-/*const btn = document.getElementById('btnEvento');
-btn.onclick = function () {
-    document.body.style.backgroundColor = "blue";
-}
-
-btn.onmouseover = function () {
-    document.body.style.backgroundColor = "red";
-}
-
-btn.onmouseleave = function () {
-    document.body.style.backgroundColor = "green";
-}
-
-
-const p = document.getElementById('mouserOver');
-p.onmouseover = logMouseOver;
-p.onmouseout = logMouseOut;
-function logMouseOver() {
-    p.innerHTML = 'MOUSE OVER detected';
-    p.style.color = "red";
-}
-
-function logMouseOut() {
-    p.innerHTML = 'MOUSE OUT detected';
-    p.style.color = "blue";
-}*/
-
-
-
-function totalSummary(){
-
+     //Variable para tomar la cantidad ingresada de tickets.
     let totalTickets = document.getElementById("tickets").value
+    
+    //Variable para crear el valor del ticket según la cantidad.
     let precioTicket = 200 * totalTickets
+    
+    //Variable para tomar la categoría seleccionada.
     let category = document.getElementById("category").value
 
+    //Condición para evaluar la categoría y aplicar el descuento en el total según la selección.
     if (category == document.getElementById("student").value) {
 
-        let descuento = precioTicket * 80 / 100 
+        let descuento = precioTicket * 80 / 100
         let precioTotal = parseInt(precioTicket - descuento)
 
         document.getElementById("total-summary").innerHTML = `Total a pagar: $${precioTotal}`
 
-    } else if (category == document.getElementById("trainee").value){
+    } else if (category == document.getElementById("trainee").value) {
 
-        let descuento = precioTicket * 50 / 100 
+        let descuento = precioTicket * 50 / 100
         let precioTotal = parseInt(precioTicket - descuento)
 
         document.getElementById("total-summary").innerHTML = `Total a pagar: $${precioTotal}`
 
-    } else if (category == document.getElementById("junior").value) { 
+    } else if (category == document.getElementById("junior").value) {
 
-        let descuento = precioTicket * 30 / 100 
+        let descuento = precioTicket * 30 / 100
         let precioTotal = parseInt(precioTicket - descuento)
 
         document.getElementById("total-summary").innerHTML = `Total a pagar: $${precioTotal}`
 
-    } else if (category == document.getElementById("default").value){ 
+    } else if (category == document.getElementById("default").value) {
 
         let precioTicket = 200 * totalTickets
         let precioTotal = precioTicket
 
         document.getElementById("total-summary").innerHTML = `Total a pagar: $${precioTotal}`
-    
+
     }
 }
 
-function deleteSummary(){
+// Eliminar datos ingresados y el total en resumen.
 
+function deleteSummary() {
+
+    //Tomamos los valores ingresados y aplicamos un nuevo valor sin contenido.
     document.getElementById("category").value = document.getElementById("default").value
     document.getElementById("name").value = ""
     document.getElementById("surname").value = ""
@@ -120,5 +56,7 @@ function deleteSummary(){
 
 
     document.getElementById("total-summary").innerHTML = ""
+
+}
 
 }
